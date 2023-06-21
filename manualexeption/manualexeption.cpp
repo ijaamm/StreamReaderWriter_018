7c75608a92a79e7faad2a681cfda00f1dfaeebde
ijaamm <ijammusyaf@gmail.com>
2023-06-21 15:39:11 +0700

    commit 12

---
+++ manualexeption/manualexeption.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	try {
+		cout << "selamat belajar di prodi TI umy" << endl;
+		throw 0.5; 
+		cout << "pernyataan tidak akan dieksekusi" << endl;
+	}
+
+}
