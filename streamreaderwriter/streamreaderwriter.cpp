59c8bdae227ac81c01778c6a45243898edea2e22
ijaamm <ijammusyaf@gmail.com>
2023-06-21 15:04:10 +0700

    commit 4

--- streamreaderwriter/streamreaderwriter.cpp
+++ streamreaderwriter/streamreaderwriter.cpp
@@ -32,5 +32,19 @@ int main() {
 	infile.open("contohfile.text");
 
 	cout << endl << ">= membuka dan membaca file" << endl;
+	
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris 
+		while (getline(infile, baris))
+		{
+			//dan tampilkan di sini
+			cout << baris << '\n';
+
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
 
 }
